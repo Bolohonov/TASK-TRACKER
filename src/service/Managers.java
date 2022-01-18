7226c3e0d31c6672d4214efdae9b8b6ed1afd1be
Bolohonov <bolohonov@list.ru>
2022-01-18 19:50:40 +0400

    refactor: Переименовать интерфейс

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -4,10 +4,20 @@ import repository.*;
 
 public class Managers {
 
+
+    //private static Managers instance;
     private final TaskManager manager = new InMemoryTasksManager();
     private final TaskCreator factory = new TaskFactory();
     private final Printable printer = new TaskPrinter();
     private final TaskUpdater updater = new InMemoryTaskUpdater();
+
+//    public static Managers getInstance() {
+//        if (instance == null) {
+//            return instance = new Managers();
+//        } else {
+//            return null;
+//        }
+//    }
 
     public TaskManager getDefault() {
         return manager;
