74eac83c5d74707543067cc9464464781a4bb59d
Bolohonov <bolohonov@list.ru>
2022-01-27 13:57:33 +0400

    fix: Исправить ошибки InMemoryHistoryManager

--- src/repository/InMemoryHistoryManager.java
+++ src/repository/InMemoryHistoryManager.java
@@ -8,116 +8,124 @@ import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private static final Map<Integer, Node> historyMap = new HashMap();
-    private static final LinkedList<Node> historyList = new LinkedList<>();
-
-    private Node last;
-    private Node first;
-    private int size = 0;
-
     @Override
     public void add(Task task) {
         if (task != null) {
-            linkLast(task);
+            LinkedUniqueList.linkLast(task);
         }
-        size++;
     }
 
     @Override
     public void remove(int id) {
-        if (historyMap.containsKey(id)) {
-            try {
-                EpicTask epic = (EpicTask) historyMap.get(id).getTask();
-                Map<Integer, SubTask> subTaskMap = epic.getSubTasksMap();
-                for (Integer subTaskId : subTaskMap.keySet()) {
-                    if (historyMap.containsKey(subTaskId)) {
-                        removeNode(historyMap.get(subTaskId));
-                    }
-                }
-            } catch (ClassCastException exp) {
-            }
-            removeNode(historyMap.get(id));
-        } else {
-            System.out.println("В истории отсутствует задача с таким ID!");
-        }
+        LinkedUniqueList.remove(id);
     }
 
     @Override
     public List<Task> getHistory() {
-        return getTasks();
+        return LinkedUniqueList.getTasks();
     }
 
     @Override
     public void clearHistory() {
-        historyMap.clear();
-        historyList.clear();
+        LinkedUniqueList.clear();
     }
 
-    private void linkLast(Task task) {
-        int id = task.getId();
-        if (historyMap.containsKey(id)) {
-            removeNode(historyMap.get(id));
-        }
-        Node l = last;
-        Node newNode = new Node(task, l, null);
-        last = newNode;
-        if (l == null) {
-            first = newNode;
-            historyMap.put(id, newNode);
-            historyList.addFirst(newNode);
-        } else {
-            l.setNextNode(newNode);
-            historyMap.put(id, newNode);
-            historyList.addLast(newNode);
-        }
-    }
+    static final class LinkedUniqueList {
+        private static final Map<Integer, Node> historyMap = new HashMap();
+        private static Node last;
+        private static Node first;
 
-    private ArrayList<Task> getTasks() {
-        ArrayList<Task> historyArrayList = new ArrayList<>();
-        historyArrayList.clear();
-        if (historyList == null || historyList.isEmpty()) {
-            System.out.println("Истории пока нет");
-            return null;
-        } else {
-            Node node = first;
-            for (int i = 0; i < size; i++) {
-                historyArrayList.add(i, node.getTask());
-                node = node.getNextNode();
+
+        private static void removeNode(Node node) {
+            if (!node.equals(first) && !node.equals(last)) {
+                Node prevNode = node.getPrevNode();
+                Node nextNode = node.getNextNode();
+                prevNode.setNextNode(nextNode);
+                nextNode.setPrevNode(prevNode);
+                historyMap.remove(node.getTask().getId());
+            } else if (node.equals(first) && node.getNextNode() != null) {
+                Node nextNode = node.getNextNode();
+                nextNode.setPrevNode(null);
+                first = nextNode;
+                historyMap.remove(node.getTask().getId());
+            } else if (node.equals(last) && node.getPrevNode() != null) {
+                Node prevNode = node.getPrevNode();
+                prevNode.setNextNode(null);
+                last = prevNode;
+                historyMap.remove(node.getTask().getId());
+            } else {
+                last = null;
+                first = null;
+                historyMap.remove(node.getTask().getId());
+            }
+            unlink(node);
+        }
+
+        private static void linkLast(Task task) {
+            int id = task.getId();
+            if (historyMap.containsKey(id)) {
+                removeNode(historyMap.get(id));
+            }
+            Node l = last;
+            Node newNode = new Node(task, l, null);
+            last = newNode;
+            if (l == null) {
+                first = newNode;
+                historyMap.put(id, newNode);
+                historyList.addFirst(newNode);
+            } else {
+                l.setNextNode(newNode);
+                historyMap.put(id, newNode);
+                historyList.addLast(newNode);
             }
         }
-        return historyArrayList;
-    }
 
-    private void removeNode(Node node) {
-        if (!node.equals(first) && !node.equals(last)) {
-            Node prevNode = node.getPrevNode();
-            Node nextNode = node.getNextNode();
-            prevNode.setNextNode(nextNode);
-            nextNode.setPrevNode(prevNode);
-            historyMap.remove(node.getTask().getId());
-        } else if (node.equals(first) && historyList.stream().iterator().hasNext()) {
-            Node nextNode = node.getNextNode();
-            nextNode.setPrevNode(null);
-            first = nextNode;
-            historyMap.remove(node.getTask().getId());
-        } else if (node.equals(last) && historyList.listIterator(size).hasPrevious()) {
-            Node prevNode = node.getPrevNode();
-            prevNode.setNextNode(null);
-            last = prevNode;
-            historyMap.remove(node.getTask().getId());
-        } else {
-            last = null;
-            first = null;
-            historyMap.remove(node.getTask().getId());
+        private static ArrayList<Task> getTasks() {
+            ArrayList<Task> historyArrayList = new ArrayList<>();
+            historyArrayList.clear();
+            if (historyMap == null || historyMap.isEmpty()) {
+                System.out.println("Истории пока нет");
+                return null;
+            } else {
+                Node node = first;
+                for (int i = 0; i < size; i++) {
+                    historyArrayList.add(i, node.getTask());
+                    node = node.getNextNode();
+                }
+            }
+            return historyArrayList;
         }
-        unlink(node);
-        size--;
-    }
 
-    private void unlink(Node node) {
-        node.setNextNode(null);
-        node.setPrevNode(null);
-        node.setTask(null);
+        private static void unlink(Node node) {
+            node.setNextNode(null);
+            node.setPrevNode(null);
+            node.setTask(null);
+        }
+
+        private static void remove(int id) {
+            if (historyMap.containsKey(id)) {
+                if (historyMap.get(id).getTask() instanceof EpicTask) {
+
+                }
+//            try {
+//                EpicTask epic = (EpicTask) historyMap.get(id).getTask();
+//                Map<Integer, SubTask> subTaskMap = epic.getSubTasksMap();
+//                for (Integer subTaskId : subTaskMap.keySet()) {
+//                    if (historyMap.containsKey(subTaskId)) {
+//                        removeNode(historyMap.get(subTaskId));
+//                    }
+//                }
+//            } catch (ClassCastException exp) {
+//            }
+                removeNode(historyMap.get(id));
+            } else {
+                System.out.println("В истории отсутствует задача с таким ID!");
+            }
+        }
+
+        private static void clear() {
+            historyMap.clear();
+        }
     }
 }
 
