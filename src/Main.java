3eba3616864a2c5932d2989b00e08963ff4412af
Bolohonov <bolohonov@list.ru>
2022-02-03 16:11:02 +0400

    feat: Add Controller

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import service.CommandManager;
-
-public class Main {
-    public static void main(String[] args) {
-        CommandManager commandManager = new CommandManager();
-        commandManager.getCommandPanel();
-    }
-}
-
