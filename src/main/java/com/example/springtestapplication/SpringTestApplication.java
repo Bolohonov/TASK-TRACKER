59371f8a3eb806705db3ef58024f9a07461818a7
Bolohonov <bolohonov@list.ru>
2022-02-03 16:12:17 +0400

    feat: Add Controller

---
+++ src/main/java/com/example/springtestapplication/SpringTestApplication.java
@@ -0,0 +1,27 @@
+package com.example.springtestapplication;
+
+import exception.ValidationException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication(scanBasePackages = {"com.example.springtestapplication", "controller"})
+@ComponentScan({"com.example.springtestapplication", "controller"})
+public class SpringTestApplication implements WebMvcConfigurer {
+
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/css/", "classpath:/static/js/"
+	};
+
+	public static void main(String[] args) throws ValidationException {
+		SpringApplication.run(SpringTestApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+	}
+}
---
+++ src/main/java/service/UsersService.java
@@ -0,0 +1,17 @@
+package service;
+
+import dto.UsersDto;
+import exception.ValidationException;
+
+import java.util.List;
+
+public interface UsersService {
+
+    UsersDto saveUser(UsersDto usersDto) throws ValidationException;
+
+    void deleteUser(Integer userId);
+
+    UsersDto findByLogin(String login);
+
+    List<UsersDto> findAll();
+}
