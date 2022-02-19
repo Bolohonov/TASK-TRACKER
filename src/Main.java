48e5637d786cfeec81e503ceb0049d955ef878a2
Bolohonov <bolohonov@list.ru>
2022-02-19 18:28:29 +0400

    feat: Добавить тестовый код

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,7 @@ public class Main {
         Task t7 = new SubTask(t5,"name7", "desc7", f.getId());
         Task t8 = new SubTask(t2,"name8", "desc8", f.getId());
         Task t9 = new SubTask(t2,"name9", "desc9", f.getId());
+        Task t10 = new EpicTask("name10", "desc10", f.getId());
 
         f.putTask(t);
         f.putTask(t2);
@@ -37,6 +38,7 @@ public class Main {
         f.putTask(t7);
         f.putTask(t8);
         f.putTask(t9);
+        f.putTask(t10);
         f.getTaskById(2);
         f.getTaskById(3);
         f.getTaskById(1);
@@ -48,23 +50,28 @@ public class Main {
         f.getTaskById(5);
         f.removeTaskById(6);
         f.removeTaskById(5);
-        FileBackedTasksManager f1 = new FileBackedTasksManager(file);
-        f1.loadFromFile(file);
+        FileBackedTasksManager f2 = new FileBackedTasksManager(file);
+        f2.loadFromFile(file);
         System.out.println("Проверка истории! (Эпик 5 и его подзадачи 6,7 - удалены)");
-        manager.getPrinter().printList(f1.getHistory());
+        manager.getPrinter().printList(f2.getHistory());
         System.out.println("-------------------------------------");
         System.out.println("Проверка эпиков!");
-        manager.getPrinter().printMap(f1.getEpicTasks());
+        manager.getPrinter().printMap(f2.getEpicTasks());
         System.out.println("-------------------------------------");
         System.out.println("Проверка подзадач!");
-        manager.getPrinter().printMap(f1.getSubTasksByEpic(f1.getTaskById(2)));
+        manager.getPrinter().printMap(f2.getSubTasksByEpic(f2.getTaskById(2)));
         System.out.println("-------------------------------------");
         System.out.println("Проверка выгрузки отдельных задач!");
-        manager.getPrinter().printTask(f1.getTaskById(2));
-        manager.getPrinter().printTask(f1.getTaskById(1));
-        manager.getPrinter().printTask(f1.getTaskById(3));
-        manager.getPrinter().printTask(f1.getTaskById(6));
-
+        manager.getPrinter().printTask(f2.getTaskById(2));
+        manager.getPrinter().printTask(f2.getTaskById(1));
+        manager.getPrinter().printTask(f2.getTaskById(3));
+        manager.getPrinter().printTask(f2.getTaskById(6));
+        manager.getPrinter().printTask(f2.getTaskById(10));
+        System.out.println("-------------------------------------");
+        FileBackedTasksManager f3 = new FileBackedTasksManager(file);
+        f3.loadFromFile(file);
+        System.out.println("Еще проверка истории!");
+        manager.getPrinter().printList(f3.getHistory());
     }
 }
 
