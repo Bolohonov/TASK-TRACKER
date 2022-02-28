e6fb96301fb7663c0e920ae571e111a95d7bb9c2
Bolohonov <bolohonov@list.ru>
2022-02-28 18:38:43 +0400

    feat: Добавить JUnit тесты

---
+++ src/test/java/InMemoryHistoryManagerTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import repository.InMemoryHistoryManager;
+
+public class InMemoryHistoryManagerTest {
+
+
+
+
+}
--- src/test/java/TaskManagerTest.java
+++
@@ -1,6 +0,0 @@
-package test.java;
-import org.junit.jupiter.api.Test;
-
-
-public class TaskManagerTest {
-}
