f54f32f88189caef445af5c4794a8db4169cafd0
Bolohonov <bolohonov@list.ru>
2022-03-22 17:51:20 +0400

    feat: Add HttpServer & HttpClient

--- src/main/java/services/HttpTaskServer.java
+++ src/main/java/services/HttpTaskServer.java
@@ -8,6 +8,7 @@ import repository.Managers;
 import repository.TaskManager;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
@@ -19,7 +20,6 @@ public class HttpTaskServer {
     public HttpTaskServer() throws IOException {
     }
 
-    ;
     private static Managers managers;
 
     static {
@@ -43,15 +43,109 @@ public class HttpTaskServer {
     static class TaskHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange httpExchange) throws IOException {
+            String response;
+            String method = httpExchange.getRequestMethod();
 
+            switch(method) {
+                case "GET":
+                    switch(getBodyFromGetRequest(httpExchange)) {
+                        case "getSingleTasks":
+                            managerToFile.getSingleTasks();
+                            break;
+                        case "getEpicTasks":
+                            managerToFile.getEpicTasks();
+                            break;
+                        case "getHistory":
+                            managerToFile.getHistory();
+                            break;
+                        case "getTaskById":
+                            String path = httpExchange.getRequestURI().getPath();
+                            String [] parameters = path.split("/");
+                            try {
+                                managerToFile.getTaskById(Integer.parseInt(parameters[3]));
+                            } catch (ManagerSaveException | NumberFormatException e) {
+                                System.out.println(e.getMessage() + " " + e.getStackTrace());
+                            }
+                            break;
+                        case "getSubTasksByEpic":
+                            path = httpExchange.getRequestURI().getPath();
+                            parameters = path.split("/");
+                            try {
+                                managerToFile.getSubTasksByEpic(managerToFile
+                                        .getTaskById(Integer.parseInt(parameters[4])));
+                            } catch (ManagerSaveException | NumberFormatException e) {
+                                System.out.println(e.getMessage() + " " + e.getStackTrace());
+                            }
+                            break;
+                        default:
+                            ////////////////////////
+                    }
+                    break;
+                case "POST":
+                    getBody(httpExchange);
 
-            String response = null;
-
+                    // извлеките заголовок
+                    boolean flag = false;
+                    Headers requestHeaders = httpExchange.getRequestHeaders();
+                    if (requestHeaders.keySet().contains("X-Wish-Good-Day=true")) {
+                        flag = true;
+                    }
+                    for (List<String> l : requestHeaders.values()) {
+                        if (l.stream().anyMatch(o -> o.equals("X-Wish-Good-Day=true"))) {
+                            flag = true;
+                        }
+                        //List<String> wishGoodDay = ...
+                    }
+                    // соберите ответ
+                    if (flag) {
+                        response = body + ", " + profession + " " + name + "!" + " Хорошего дня!";
+                    } else {
+                        response = body + ", " + profession + " " + name + "!";
+                    }
+                    break;
+                case "DELETE":
+                    response = "Здравствуйте!";
+                    break;
+                default:
+                    response = "Некорректный метод!";
+                    break;
+            }
 
             httpExchange.sendResponseHeaders(200, 0);
             try (OutputStream os = httpExchange.getResponseBody()) {
                 os.write(response.getBytes());
             }
+        }
+
+        private String getBodyFromGetRequest(HttpExchange httpExchange) throws IOException {
+            String command = null;
+            InputStream input = httpExchange.getRequestBody();
+            String body = new String(input.readAllBytes());
+            String path = httpExchange.getRequestURI().getPath();
+            String [] parameters = path.split("/");
+            if (parameters.length == 3 && parameters[2].equals("task")) {
+                command = "getSingleTasks";
+            } else {
+                if (parameters.length == 3 && parameters[2].equals("epic")) {
+                    command = "getEpicTasks";
+                } else {
+                    if (parameters.length == 3 && parameters[2].equals("history")) {
+                        command = "getHistory";
+                    }
+                }
+            }
+            if (parameters.length == 4) {
+                command = "getTaskById";
+            }
+            if (parameters.length == 5 && parameters[2].equals("subtask") && parameters[3].equals("epic")) {
+                command = "getSubTasksByEpic";
+            }
+            return command;
+        }
+
+        private String getBodyFromPostRequest(HttpExchange httpExchange) throws IOException {
+            String command = null;
+            return command;
         }
     }
 }
