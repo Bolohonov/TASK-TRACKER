a892fadd9b351dbabca8b3f2b11ba9d52de747fc
Bolohonov <bolohonov@list.ru>
2022-03-24 18:24:36 +0400

    feat: Add HttpServer & HttpClient

--- src/main/java/repository/Managers.java
+++ src/main/java/repository/Managers.java
@@ -4,19 +4,19 @@ import java.nio.file.Paths;
 
 public class Managers {
     private final TaskManager manager = new InMemoryTasksManager();
-    private final TaskManager httpTaskManager = new HTTPTaskManager(Paths
-            .get("https://localhost:8079"));
+//    private final TaskManager httpTaskManager = new HTTPTaskManager(Paths
+//            .get("/localhost"));
     private final TaskManager managerToFile =
-            new FileBackedTasksManager(Paths.get("./resources/data.csv"));
+            new FileBackedTasksManager(Paths.get("C:/WORK/Git/task-tracker-sprint7-hw/resources/data.csv"));
     private final TaskCreator factory = new TaskFactory();
     private final TaskUpdater updater = new InMemoryTaskUpdater();
 
     public Managers() throws ManagerSaveException {
     }
 
-    public TaskManager getDefault() {
-        return httpTaskManager;
-    }
+//    public TaskManager getDefault() {
+//        return httpTaskManager;
+//    }
 
     public TaskManager getInMemoryTasksManager() {
         return manager;
